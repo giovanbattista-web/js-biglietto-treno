a36ab31f05b86fcfafc4cb52f77d3df431010c5c
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-03-17 18:18:52 +0100

    [UPD] MODIFICA EFFETTUATA AL PREZZO

--- js/javascript.js
+++ js/javascript.js
@@ -3,5 +3,5 @@ let numeroKm = parseInt(prompt("Inserisci il numero di km da percorrere"));
 let anniUtente = parseInt(prompt("Inserisci l'età"));
 
 // PREZZO DEFINITO IN BASE AI KM
-let prezzoTotale = numeroKm * 21;
+let prezzoTotale = numeroKm * 0.21;
 console.log("Il prezzo definito in base ai km è di ${prezzoTotale}");
