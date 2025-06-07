97b9bdf232b864dafaa3558b6b32b3b18d6d6451
giovanbattista-web <giovanbattistadamico1994@gmail.com>
2025-06-07 07:07:09 +0200

    [UPD] README.md AGGIORNATO

--- js/javascript.js
+++ js/javascript.js
@@ -1,22 +1,26 @@
-// DICHIARAZIONE ED INIZIALIZZAZIONE DELLE VARIABILI 
+// DICHIARO ED INIZIALIZZO 2 VARIABILI
 const numeroKm = parseInt(prompt("Inserisci il numero di km da percorrere"));
 const anniUtente = parseInt(prompt("Inserisci la tua età"));
 
 // DICHIARO UNA VARIABILE CHE CONTERRA' IL PREZZO DEL BIGLIETTO E LA INIZIALIZZO AL PREZZO BASE 
 let price = numeroKm * 0.21;
-//DICHIARO UNA VARIABILE MESSAGE A CUI ASSEGNERO' IL VALORE DURANTE IL CONTROLLO
+
+// DICHIARO UNA VARIABILE MESSAGE A CUI ASSEGNERO' IL VALORE DURANTE IL CONTROLLO
 let message;
 
-// EFFETTUARE IL CONTROLLO SULL'ETA' INSERITA DALL'UTENTE
+// EFFETTUO IL CONTROLLO SULL'ETA' INSERITA DALL'UTENTE
 if(anniUtente < 18 ){ 
+                   
                    // EFFETTUO IL CALCOLO DEL PREZZO SCONTATO DEL 20%
                    price = price - price * 0.2;
+                   
                    // SCRIVO IL MESSAGGIO DA MOSTRARE A VIDEO E LO ASSEGNO ALLA VARIABILE MESSAGE
                    message = `Hai diritto ad uno sconto del 20%. Il prezzo del tuo biglietto è di ${price.toFixed(2)} €`;
                     }
 else if (anniUtente > 65){
                    // EFFETTUO IL CALCOLO DEL PREZZO SCONTATO DEL 40%
                    price = price - price * 0.4;
+                   
                    // SCRIVO IL MESSAGGIO DA MOSTRARE A VIDEO E LO ASSEGNO ALLA VARIABILE MESSAGE
                    message = `Hai diritto ad uno sconto del 40%. Il prezzo del tuo biglietto è di ${price.toFixed(2)} €`;
                     }
@@ -26,6 +30,7 @@ else
       // SCRIVO IL MESSAGGIO DA MOSTRARE A VIDEO E LO ASSEGNO ALLA VARIABILE MESSAGE  
       message = `Il prezzo del tuo biglietto è ${price.toFixed(2)} €`;
    }
-console.log(message);
+
+   console.log(message);
 
 
